137b4e039ed3706eafe6b833ce60fccd680c2073
jlee2967 <jlee2967@uwo.ca>
2017-03-03 23:36:11 -0500

    Update user-page.js

--- EventileReact/src/main/js/user-page.js
+++ EventileReact/src/main/js/user-page.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import 'whatwg-fetch';
 import auth from './auth';
 
+// Currently, this page does not functionally work since no value is being read from the
+// user information. The path was create just for navigation.
+
 class UserPage extends React.Component {
     constructor() {
         super();
