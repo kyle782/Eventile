925400a8dc2d77134fbf8e4f8486969ce184af2b
Frank Jiao <frankzjiao@gmail.com>
2017-03-22 19:24:55 -0400

    ssss

--- EventileReact/grails-app/controllers/grails3/example/CommentController.groovy
+++ EventileReact/grails-app/controllers/grails3/example/CommentController.groovy
@@ -1,7 +1,6 @@
 package grails3.example
 
 class CommentController {
-<<<<<<< HEAD
    def save (){
 
    }
@@ -11,19 +10,4 @@ class CommentController {
    }
 
    def index() { }
-=======
-    def edit(){
-        def comment = Comment.get(params.id)
-        comment = new Comment()
-        System.out.println()
-    }
-
-    def save(){
-        def comment = loadPost(params.id)
-        comment.properties = params
-    }
-
-    def index(){
-    }
->>>>>>> 786ff47e010fdb16a5dc76808a8ea5009a86256a
 }
